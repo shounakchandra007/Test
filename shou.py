194702b9b1f1d8b578ec582aedabb3d57ce47a80
Shounak Chandra <56895242+shounakchandra007@users.noreply.github.com>
2025-01-25 04:34:44 +0000

    final commit

--- shou.py
+++ shou.py
@@ -1,1 +1,9 @@
-print("Hello WOrld")
+import json
+
+def lambda_functions():
+    #defies functions
+    response = {
+        "statusCode": 200,
+        "body": json.dumps({"message": "Hello from Shounak's Lambda Function"})
+    }
+    return response
